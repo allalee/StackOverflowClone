1af285c136320ca6b03ee77f255743b536dd0362
allalee <allan.lee@stonybrook.edu>
2019-03-23 14:38:52 -0400

    Completed prototype question adding. It does not take media IDs into account and ID generation may need changing in future

--- stackoverflowclone/main.py
+++ stackoverflowclone/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from flask import Flask, request, render_template, jsonify, session, make_response, redirect, url_for, abort
-import datetime, string, json, pymongo
+import datetime, string, json, pymongo, time
 
 db_connection = pymongo.MongoClient("mongodb://localhost:27017/")
 database = db_connection.UserAccounts
 account_system = database.accounts #Refer to the accounts collection in the database
+account_questions = database.account_questions #Refers to the data of the questions asked by each user
 
 app = Flask(__name__, template_folder='templates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -41,6 +42,33 @@ def logout():
 		return jsonify({"status": "error", "error": "Not signed in!"})
 	session.clear()
 	return jsonify({"status": "OK", "error": ""})
+
+@app.route("/questions/add", methods=['GET', 'POST'])
+def add_question():
+	if(request.method == 'GET'):
+		return render_template('add_question.html')
+	else:
+		if not('username' in session):
+			return jsonify({"status": "error","id": "", "error": "User not logged in!"})
+		else:
+			request_json = request.get_json()
+			question_count = account_questions.count() + 1 #Will be our unique ID for now...
+			question_count = str(question_count) #IDs must be a string
+			question = {}
+			question["id"] = question_count
+			question["user"] = {"username": session['username'], "reputation": 1} #Default reputation is set to 1 currently. Will change in the future
+			question["title"] = request_json['title']
+			question["body"] = request_json['body']
+			question["score"] = 0
+			question["view_count"] = 0
+			question["answer_count"] = 0
+			question["timestamp"] = int(time.time())
+			question["media"] = "" #Currently do not support media tags in the version
+			question["tags"] = request_json["tags"].split(",")
+			question["accepted_answer_id"] = "Null"
+			account_questions.insert(question)
+			return jsonify({"status": "OK", "id": question_count, "error": ""})
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=3001)
---
+++ stackoverflowclone/static/js/add_question.js
@@ -0,0 +1,23 @@
+function submit(){
+	var q_title = document.getElementById("title").value
+	var q_body = document.getElementById("body").value
+	var q_tags = document.getElementById("tags").value
+	$.ajax({
+		type: "POST",
+		url: "/questions/add",
+		data: JSON.stringify({title: q_title, body: q_body, tags: q_tags}),
+		contentType: "application/json; charset=utf-8",
+		success: function(data){
+			check(data)
+		},
+		dataType: "json"
+	});
+}
+
+function check(data){
+	if(data.status == "error"){
+		alert(data.error)
+	} else {
+		alert("Your question has been posted")
+	}
+}
