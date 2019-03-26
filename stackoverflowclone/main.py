61d04aadc85e61b06a17928f9992e4a623052afc
allalee <allan.lee@stonybrook.edu>
2019-03-25 21:00:00 -0400

    Another attempt

--- stackoverflowclone/main.py
+++ stackoverflowclone/main.py
@@ -39,13 +39,13 @@ def login():
 	else:
 		return render_template('login.html', logged_out=True)
 
-@app.route("/static/js/<path>", methods=['GET'])
+@app.route("/js/<path>", methods=['GET'])
 def get_static_js_files(path):
-	return send_from_directory("js", path)
+	return send_from_directory("static/js", path)
 
-@app.route("/static/css/<path>", methods=['GET'])
+@app.route("/css/<path>", methods=['GET'])
 def get_static_css_files(path):
-	return send_from_directory("css", path)
+	return send_from_directory("static/css", path)
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
