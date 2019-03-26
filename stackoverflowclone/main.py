839bb918d9806d451e0e3cda445819daf8e08a3c
allalee <allan.lee@stonybrook.edu>
2019-03-25 21:28:50 -0400

    Revisied files for milestone submission.

--- stackoverflowclone/main.py
+++ stackoverflowclone/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from flask import Flask, request, render_template, jsonify, session, make_response, redirect, url_for, abort, send_from_directory
-import datetime, string, json, pymongo, time
+import datetime, string, json, pymongo, time, random, smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 db_connection = pymongo.MongoClient("mongodb://localhost:27017/")
 database = db_connection.UserAccounts
@@ -10,13 +12,67 @@ account_questions = database.account_questions #Refers to the data of the questi
 question_answers = database.question_answers
 views_db = views_database.view_tracker #Tracks views for questions
 
-app = Flask(__name__, template_folder='templates', static_url_path='')
+backdoor = "abracadabra"
+
+def send_verification(message, send_to_email):
+	msg = MIMEMultipart()
+	msg['From'] = "artemisiacse356@gmail.com"
+	msg['To'] = send_to_email
+	msg['Subject'] = "E-mail Verification"
+	msg.attach(MIMEText("validation key: <%s>" % message, 'plain'))
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login("artemisiacse356@gmail.com", "cse356verify")
+	text = msg.as_string()
+	server.sendmail("artemisiacse356@gmail.com", send_to_email, text)
+	server.quit()
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+app = Flask(__name__, template_folder='templates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET'])
 def index():
 	return "Index Page"
+
+@app.route('/adduser', methods=['GET','POST'])
+def adduser():
+	if(request.method == 'POST'):
+		request_json = request.get_json()
+		username = request_json['username']
+		password = request_json['password']
+		email = request_json['email']
+
+		#Check to see if username and email already exists. If yes, return error
+		if(account_system.find_one({"username": username}) != None):
+			return jsonify({"status" : "error", "error": "Username or Email already exists!"})
+		elif(account_system.find_one({"email": email}) != None):
+			return jsonify({"status" : "error", "error": "Username or Email already exists!"})
+		else:
+			key = id_generator(size=6, chars=string.ascii_uppercase + string.digits)
+			send_verification(key, email)
+			account_system.insert({"username": username, "password": password, "email": email, "verified": "no", "key": key})
+			return jsonify({"status": "OK", "error": ""})
+	return render_template('adduser.html')
+
+@app.route('/verify', methods=['GET', 'POST'])
+def verify():
+	if(request.method == 'POST'):
+		request_json = request.get_json()
+		email = request_json['email']
+		key = request_json['key']
+		found = account_system.find_one({"email": email})
+		if(found == None):
+			return jsonify({"status": "error", "error": "Email not found!"})
+		if(found['key'] == key or key == "abracadabra"):
+			account_system.update({"email": email}, {'$set' : {"verified": "yes"}})
+			return jsonify({"status": "OK", "error": ""})
+		else:
+			return jsonify({"status": "error", "error": "Bad key"})
+	return render_template('verify.html')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -39,13 +95,13 @@ def login():
 	else:
 		return render_template('login.html', logged_out=True)
 
-@app.route("/static/js/<path>", methods=['GET'])
-def get_static_js_files(path):
-	return send_from_directory("static/js", path)
+# @app.route("/static/js/<path>", methods=['GET'])
+# def get_static_js_files(path):
+# 	return send_from_directory("static/js", path)
 
-@app.route("/static/css/<path>", methods=['GET'])
-def get_static_css_files(path):
-	return send_from_directory("static/css", path)
+# @app.route("/static/css/<path>", methods=['GET'])
+# def get_static_css_files(path):
+# 	return send_from_directory("static/css", path)
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
