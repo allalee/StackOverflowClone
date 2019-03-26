441be6a417fb328dcb6aa1bc49ce3d675e66e5e8
allalee <allan.lee@stonybrook.edu>
2019-03-25 21:30:01 -0400

    Added files for submission

---
+++ stackoverflowclone/static/js/user.js
@@ -0,0 +1,42 @@
+function submit(){
+	var usrname = document.getElementById("username").value
+	var passwd = document.getElementById("password").value
+	var e_mail = document.getElementById("email").value
+	$.ajax({
+		type: "POST",
+		url: "/adduser",
+		data: JSON.stringify({username:usrname,password: passwd,email:e_mail }),
+		contentType: "application/json; charset=utf-8",
+		success: function(data){
+			redirect(data)
+		},
+		dataType: "json"
+	});
+}
+
+function redirect(data){
+	if(data.status == "error"){
+		alert("Username or email already exists!")
+	} else {
+		window.location.href = "http://64.52.86.192:3000/verify";
+	}
+}
+
+
+function verify(){
+	var e_mail = document.getElementById("email").value
+	var verify_key = document.getElementById("key").value
+	console.log(verify_key)
+	$.ajax({
+		type: "POST",
+		url: "/verify",
+		data: JSON.stringify({email: e_mail, key: verify_key}),
+		contentType: "application/json; charset=utf-8",
+		success: redirect_login(),
+		dataType: "json"
+	})
+}
+
+function redirect_login(){
+	window.location.href = "http://64.52.86.192:3001/login"
+}
